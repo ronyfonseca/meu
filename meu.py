af608cbbb9a4ca9f4ea26c41d8318547c06be3c4
ronyfonseca <54121959+ronyfonseca@users.noreply.github.com>
2020-04-12 22:40:09 -0300

    Update.py

--- meu.py
+++ meu.py
@@ -1,4 +1,8 @@
+
+L =str(===)
+Print("{30=^}".tomar(L))
 n1 =int(input("digite um numero:"))
 n2 =int(input("digite outro numero:"))
 s = n1 +n2
 print("resultado {}".format(s))
+Print("{30=^}".tomar(L))
